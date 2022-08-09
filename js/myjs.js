fed00969005afb1bc25eb4ce2605538fad6a17c5
maximeabade <maximeabade@gmail.com>
2022-08-09 17:04:32 +0200

    difficult random password generator working

--- js/myjs.js
+++ js/myjs.js
@@ -1,5 +1,6 @@
-var number_of_chars = 10;
+var number_of_chars = 100;
 var myWord = "";
+var myString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&é(-è_çà)=*/-.,;:!§~#{[|`\^@]}`]}^¨$£¤µ*%ù"
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -7,12 +8,14 @@ function getRandomInt(max) {
 
 function randomGenerator() {
     for (var i=0 ; i<number_of_chars ; i++) {
-        myWord += (getRandomInt(128) + "\n");
+        myWord += myString.charAt(getRandomInt(myString.length));
         console.log(myWord);
     }
+    var retour = myWord;
     var body = document.getElementById("put_it_here");
     var myTextContainer = document.createElement("p");
     myTextContainer.textContent = myWord;
     body.appendChild(myTextContainer);
     myWord = "";
+    return retour;
 }
